2ef7331f1fe9932d63e591dbf6f91db5680f3a02
cheglok <cheglokm@yandex.ru>
2022-11-03 22:05:09 +0600

    settings popup is dragguble. add 'use strict' to each module file

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,4 @@
+'use strict';
 (function () {
   // Поиск наибольшего значения в массиве
   var getMaxFromArr = function (arr) {
--- js/wizard-color-settings.js
+++ js/wizard-color-settings.js
@@ -1,3 +1,4 @@
+'use strict';
 (function () {
   var availableCoatColors = [
     'rgb(101, 137, 164)',
