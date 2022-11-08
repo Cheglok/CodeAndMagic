16b3b6783fae60a5021ccae670d042b76b4b839f
cheglok <cheglokm@yandex.ru>
2022-11-08 12:14:07 +0600

    data for similar wizards is downloaded from server and form sends data to server via xmlhttprequest

--- js/backend.js
+++ js/backend.js
@@ -1,26 +1,30 @@
 'use strict';
 (function () {
   var DOWNLOAD_URL = 'https://24.javascript.pages.academy/code-and-magick/data';
-  var UPLOAD_URL = 'https://24.javascri pt.pages.academy/code-and-magick';
+  var UPLOAD_URL = 'https://24.javascript.pages.academy/code-and-magick';
+
+  var addEventListeners = function (onSuccess, onError, request) {
+    request.addEventListener('load', function () {
+      if (request.status === 200) {
+        onSuccess(request.response);
+      } else {
+        onError('Статус ответа: ' + request.status + ' ' + request.statusText);
+      }
+    });
+
+    request.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    request.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться в течение ' + request.timeout + 'мс');
+    })
+  }
 
   var download = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться в течение ' + xhr.timeout + 'мс');
-    })
+    addEventListeners(onSuccess, onError, xhr);
 
     xhr.timeout = 10000;
     xhr.open('GET', DOWNLOAD_URL);
@@ -28,26 +32,11 @@
   };
 
   var upload = function (data, onSuccess, onError) {
+    console.log(data)
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function () {
-      switch (xhr.status) {
-        case 200:
-          onSuccess(xhr.response);
-          break;
-        default:
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться в течение ' + xhr.timeout + 'мс');
-    })
+    addEventListeners(onSuccess, onError, xhr);
 
     xhr.open('POST', UPLOAD_URL);
     xhr.send(data);
